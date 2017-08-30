a7d6fe17e60990e4ce5c46fa38982c8f1fe35953
SSsEnzo <junho0709@hotmail.com>
2017-08-30 16:20:06 +0900

    file moved

---
+++ week3/daily_toy_week3_wed.js
@@ -0,0 +1,7 @@
+function convertDoubleSpaceToSingle(str) {
+  return str.split("  ").join(" ")
+}
+
+
+var output = convertDoubleSpaceToSingle("string  with  double  spaces");
+console.log(output); // --> "string with double spaces"
